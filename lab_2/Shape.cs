dd48c47482800b8fd54aae941222079fdf5622f3
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-05-29 17:05:59 +0200

    lab 2: init

---
+++ lab_2/Shape.cs
@@ -0,0 +1,12 @@
+﻿namespace lab_2 {
+
+    internal abstract class Shape {
+        abstract public double Square();
+        abstract public double Perimeter();
+        abstract public double CenterOfMass();
+
+        protected double _square { get; set; }
+        protected double _perimeter { get; set; }
+        protected Point _center { get; set; }
+    }
+}
