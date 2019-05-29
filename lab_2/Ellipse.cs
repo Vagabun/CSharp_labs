9de2e79c1bb328d0b95e2b904dc33b7e55b63ce4
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-05-29 21:56:27 +0200

    lab 2: add Ellipse class

---
+++ lab_2/Ellipse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lab_2 {
+    internal sealed class Ellipse : Shape {
+        private Point _focus_1 { get; set; }
+        private Point _focus_2 { get; set; }
+        private double _semiMajorAxis { get; set; }
+        private double _semiMinorAxis { get; set; }
+        private double _focalDistance { get; set; }
+        private double _e { get; set; }
+
+        public Ellipse(Point focus_1, Point focus_2, double semiMajorAxis) {
+            _focus_1 = focus_1;
+            _focus_2 = focus_2;
+            _semiMajorAxis = semiMajorAxis;
+
+            _focalDistance = Math.Sqrt(Math.Pow(Math.Abs(_focus_2.x - _focus_1.x), 2) +
+                Math.Pow(Math.Abs(_focus_2.y - _focus_1.y), 2)) / 2;
+            _e = _focalDistance / _semiMajorAxis;
+            _semiMinorAxis = _semiMinorAxis * Math.Sqrt(1 - Math.Pow(_e, 2));
+
+            perimeter = calculatePerimeter();
+            square = calculateSquare();
+            centerOfMass = calculateCenterOfMass();
+        }
+
+        public override double calculateSquare() {
+            return Math.PI * _semiMajorAxis * _semiMinorAxis;
+        }
+
+        public override double calculatePerimeter() {
+            return (4 * Math.PI * _semiMajorAxis * _semiMinorAxis +
+                Math.Pow(_semiMajorAxis - _semiMinorAxis, 2)) / (_semiMajorAxis + _semiMinorAxis);
+        }
+
+        public override Point calculateCenterOfMass() {
+            return new Point(_focus_1.x + (_focus_2.x - _focus_1.x),
+                _focus_1.y + (_focus_2.y - _focus_1.y));
+        }
+    }
+}
