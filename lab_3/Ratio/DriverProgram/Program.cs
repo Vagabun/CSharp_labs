94d73258472cd4a6d7aeadb980787551338b73ff
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-05-31 22:59:12 +0200

    lab 3: add driver program

---
+++ lab_3/Ratio/DriverProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RatioLibrary;
+
+namespace DriverProgram {
+    class Program {
+        static void Main(string[] args) {
+
+            Ratio r1 = new Ratio(3, 4);
+            Ratio r2 = new Ratio(7, 8);
+            //implicit ToString() call?
+            Console.WriteLine("{0} is {1}, {2} is {3}", r1, r1.ToDouble(), r2, r2.ToDouble());
+            Console.WriteLine("{0} + {1} = {2}", r1, r2, r1 + r2);
+            Console.WriteLine("{0} - {1} = {2}", r1, r2, r1 - r2);
+            Console.WriteLine("{0} * {1} = {2}", r1, r2, r1 * r2);
+            Console.WriteLine("{0} / {1} = {2}", r1, r2, r1 / r2);
+        }
+    }
+}
