686f110867375df5ed8222f194482ffc24af6e47
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-06-01 20:31:20 +0200

    lab 4: init

---
+++ lab_4/BitContainer/BitContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BitContainer {
+    public class BitContainer : IEnumerable<bool> {
+
+        public IEnumerator<bool> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab_4/BitContainer/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BitContainer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
