d27e5a046b74624dc5f4e6090ad71a06d8861851
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-06-01 14:44:05 +0200

    lab 3: rewrite unit tests

--- lab_3/Ratio/RatioLibrary.Tests/UnitTest1.cs
+++ lab_3/Ratio/RatioLibrary.Tests/UnitTest1.cs
@@ -1,65 +1,92 @@
 using NUnit.Framework;
-using RatioLibrary;
 
 namespace RatioLibrary.Tests {
+
     [TestFixture]
     public class Tests {
 
-        private Ratio r1;
-        private Ratio r2;
-
-        [TestCase(8, 10, ExpectedResult = 0.8)]
-        [TestCase(3, 4, ExpectedResult = 0.75)]
-        [TestCase(1, 2, ExpectedResult = 0.5)]
-        public double CreateRatio(int n, int d) {
-            return new Ratio(n, d).ToDouble();
+        [TestCase(7, 8, 7, 8)]
+        [TestCase(3, 4, 3, 4)]
+        [TestCase(1, 3, 1, 3)]
+        public void CreateRatio(int n, int d, int resN, int resD) {
+            Ratio r = new Ratio(n, d);
+            Assert.Multiple(() => {
+                Assert.AreEqual(r.Numerator, resN);
+                Assert.AreEqual(r.Denominator, resD);
+            });
         }
 
         [Test]
-        public void ExceptionAtCreate() {
-            Assert.Throws<DenominatorException>(() => new Ratio(3, 0));
-        }
+        public void ExceptionAtCreate() => Assert.Throws<DenominatorException>(() => new Ratio(3, 0));
 
         [Test]
-        public void ExceptionAtCalculate() {
-            Assert.Throws<DenominatorException>(() => {
-                var result = new Ratio(3, 4) / new Ratio(0, 5);
+        public void ExceptionAtCalculate() => Assert.Throws<DenominatorException>(() => {
+            var result = new Ratio(3, 4) / new Ratio(0, 5);
+        });
+
+        [TestCase(8, 10, 4, 5)]
+        [TestCase(21, 7, 3, 1)]
+        [TestCase(32, 64, 1, 2)]
+        public void Reduction(int n, int d, int resN, int resD) {
+            Ratio r = new Ratio(n, d);
+            Assert.Multiple(() => {
+                Assert.AreEqual(r.Numerator, resN);
+                Assert.AreEqual(r.Denominator, resD);
             });
         }
 
         [TestCase(8, 10, ExpectedResult = "4/5")]
         [TestCase(21, 7, ExpectedResult = "3/1")]
         [TestCase(32, 64, ExpectedResult = "1/2")]
-        public string Reduction(int n, int d) {
-            return new Ratio(n, d).ToString();
-        }
+        public string ToStringMethod(int n, int d) => new Ratio(n, d).ToString();
+
+        [TestCase(8, 10, ExpectedResult = 0.8)]
+        [TestCase(3, 4, ExpectedResult = 0.75)]
+        [TestCase(32, 64, ExpectedResult = 0.5)]
+        public double ToDoubleMethod(int n, int d) => new Ratio(n, d).ToDouble();
 
         [Test]
         public void Addition() {
-            r1 = new Ratio(1, 4);
-            r2 = new Ratio(2, 6);
-            Assert.AreEqual("7/12", (r1 + r2).ToString());
+            Ratio r1 = new Ratio(1, 4);
+            Ratio r2 = new Ratio(2, 6);
+            Ratio result = r1 + r2;
+            Assert.Multiple(() => {
+                Assert.AreEqual(result.Numerator, 7);
+                Assert.AreEqual(result.Denominator, 12);
+            });
         }
 
         [Test]
         public void Subtraction() {
-            r1 = new Ratio(3, 7);
-            r2 = new Ratio(1, 14);
-            Assert.AreEqual("5/14", (r1 - r2).ToString());
+            Ratio r1 = new Ratio(3, 7);
+            Ratio r2 = new Ratio(1, 14);
+            Ratio result = r1 - r2;
+            Assert.Multiple(() => {
+                Assert.AreEqual(result.Numerator, 5);
+                Assert.AreEqual(result.Denominator, 14);
+            });
         }
 
         [Test]
         public void Multiplication() {
-            r1 = new Ratio(1, 2);
-            r2 = new Ratio(1, 3);
-            Assert.AreEqual("1/6", (r1 * r2).ToString());
+            Ratio r1 = new Ratio(1, 2);
+            Ratio r2 = new Ratio(1, 3);
+            Ratio result = r1 * r2;
+            Assert.Multiple(() => {
+                Assert.AreEqual(result.Numerator, 1);
+                Assert.AreEqual(result.Denominator, 6);
+            });
         }
 
         [Test]
         public void Division() {
-            r1 = new Ratio(1, 4);
-            r2 = new Ratio(1, 2);
-            Assert.AreEqual("1/2", (r1 / r2).ToString());
+            Ratio r1 = new Ratio(1, 4);
+            Ratio r2 = new Ratio(1, 2);
+            Ratio result = r1 / r2;
+            Assert.Multiple(() => {
+                Assert.AreEqual(result.Numerator, 1);
+                Assert.AreEqual(result.Denominator, 2);
+            });
         }
     }
 }
