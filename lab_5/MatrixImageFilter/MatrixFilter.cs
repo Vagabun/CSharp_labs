c2a652a21de2e0c58a6477a7fb7bfb28f8f5408c
Vagabun <16858610+Vagabun@users.noreply.github.com>
2019-06-04 00:05:14 +0200

    lab 5: add normal processing for image

--- lab_5/MatrixImageFilter/MatrixFilter.cs
+++ lab_5/MatrixImageFilter/MatrixFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace MatrixImageFilter {
     public class MatrixFilter {
@@ -6,13 +7,18 @@ namespace MatrixImageFilter {
         private readonly int K;
         private double[,] kernel; //gaussian matrix
         private readonly Func<int, int, int, double> gaussian =
-            (x, y, sigma) => Math.Exp(-(Math.Pow(x, 2) + Math.Pow(y, 2)) / (2 * Math.Pow(sigma, 2)) / (2 * Math.PI * Math.Pow(sigma, 2);
+            (x, y, sigma) => Math.Exp(-(x * x + y * y) / (2 * sigma * sigma)) /
+            (2 * Math.PI * (sigma * sigma));
 
         public MatrixFilter(int radius) {
             this.radius = radius;
             K = 2 * radius + 1;
             kernel = new double[K, K];
             ComputeGaussianMatrix();
+        }
+
+        public Bitmap ApplyGaussianBlur(Bitmap src, bool runUnsafe) {
+            return NormalProcessing(ref src);
         }
 
         private void ComputeGaussianMatrix() {
@@ -31,6 +37,33 @@ namespace MatrixImageFilter {
                     kernel[x, y] /= sum;
         }
 
+        private Bitmap NormalProcessing(ref Bitmap image) {
+            Bitmap i = (Bitmap)image.Clone();
+
+            for (int x = 0; x < i.Width; ++x)
+                for (int y = 0; y < i.Height; ++y) {
+                    Color newPixel = ApplyKernelToPixel(x, y, i);
+                    i.SetPixel(x, y, newPixel);
+                }
+            return i;
+        }
+
+        //using formula from lab text
+        private Color ApplyKernelToPixel(int x, int y, Bitmap image) {
+            byte R = 0, G = 0, B = 0;
+            for (int i = y - radius; i <= y + radius; ++i) {
+                int n = (i < 0) ? 0 : (i >= image.Height) ? image.Height - 1 : i;
+                for (int j = x - radius; j <= x + radius; ++j) {
+                    int m = (j < 0) ? 0 : (j >= image.Width) ? image.Width - 1 : j;
+                    Color pixel = image.GetPixel(m, n);
+                    R += (byte)(pixel.R * kernel[i - y + radius, j - x + radius]);
+                    G += (byte)(pixel.G * kernel[i - y + radius, j - x + radius]);
+                    B += (byte)(pixel.B * kernel[i - y + radius, j - x + radius]);
+                }
+            }
+
+            return Color.FromArgb(R, G, B);
+        }
 
     }
 }
--- lab_5/MatrixImageFilter/Program.cs
+++ lab_5/MatrixImageFilter/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace MatrixImageFilter {
     class Program {
         static void Main(string[] args) {
-
+            MatrixFilter m = new MatrixFilter(5);
+            Bitmap img = new Bitmap("image.png");
+            Bitmap result = m.ApplyGaussianBlur(img, false);
+            result.Save("normal.png", ImageFormat.Png);
         }
     }
 }
